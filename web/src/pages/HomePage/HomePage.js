35f06b46adb3be5a54a61f38011179899e1ec941
Brian Schulte <bschulte@kryptowire.com>
2021-12-09 14:46:26 -0800

    added home page

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,24 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags
+        title="Home"
+        description="Home page for Youtube syncing service"
+      />
+
+      <h1>HomePage</h1>
+      <p>
+        Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
+      </p>
+      <p>
+        My default route is named <code>home</code>, link to me with `
+        <Link to={routes.home()}>Home</Link>`
+      </p>
+    </>
+  )
+}
+
+export default HomePage
